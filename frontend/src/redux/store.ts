be50bf09f6830d8494a72cd6a78d7a03189b8e16
nguyenvien161002 <nguyenvien161002@gmail.com>
2025-08-18 20:21:18 +0700

    feat: add new state data knight of owner, edit ui attack knight

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit"
+import KnightsOwnerReducer from "./KnightsOwnerReducer"
+import walletReducer from "./walletReducer"
 
-import walletReducer from './walletReducer';
 const store = configureStore({
-    reducer: {
-        wallet: walletReducer,
-    }
+  reducer: {
+    wallet: walletReducer,
+    knightsOwner: KnightsOwnerReducer,
+  },
 })
 
-export default store;
+export default store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
---
+++ frontend/src/type/index.ts
@@ -0,0 +1,17 @@
+export interface DataKnight {
+    _id: string
+    name: string
+    owner: string
+    knightID: number
+    tokenURI: string
+    dna: string
+    image: string
+    createdAt:string
+    updatedAt: string
+    attackTime: number
+    level: number
+    lostCount: number
+    sexTime: number
+    winCount: number
+}
+
